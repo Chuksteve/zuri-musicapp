76738efc0bb9262adc49eb0d23f96d84e8d1e912
Chuksteve <steveokonye@gmail.com>
2022-10-30 19:15:51 +0100

    update of models.py made by Chuksteve

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,6 +1,4 @@
 from datetime import datetime
-from email.policy import default
-from importlib.resources import contents
 from django.db import models
 
 # Create your models here.
@@ -10,13 +8,22 @@ class Artiste (models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
 
+    def __str__(self):
+       return self.first_name 
+
 class Song (models.Model):
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     date_released = models.DateField(default=datetime.today)
     likes = models.IntegerField()
+
+    def __str__(self):
+       return self.title
      
 class Lyric (models.Model):
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
+
+    def __str__(self):
+       return self.content
     
